21591e14484a38b7e2a9dc9a93f64a59c2088631
ilya <isagaidak@isgneuro.com>
2021-11-01 16:53:40 +0300

    loggers for osr and convert to bin added

--- jobsmanager_transit/views/makejob.py
+++ jobsmanager_transit/views/makejob.py
@@ -51,20 +51,12 @@ class MakeJobMod(APIView, BaseHandlerMod, MakeJob):
 
     def post(self, request: Request):
         remote_ip = self._get_client_ip(request.META)
-        # original_otl = self._get_original_otl(
-        #     request.data.get('original_otl', '')[0]  # why they put it in a LIST? cannot use a string pattern on a bytes-like object DECODE FIRST?
-        # )
-        # self._convert_to_binary(request.data)  # todo for testing REMOVE
-        # indexes = re.findall(r"index\s?=\s?([\"\']?_?\w*[\w*][_\w+]*?[\"\']?)", original_otl)
-        # user_accessed_indexes = [EverythingEqual()] # todo authorization
         loop = asyncio.new_event_loop()
         asyncio.set_event_loop(loop)
         response = loop.run_until_complete(
-            # зовем продюсера: который передает словари
             self.jobs_manager.jobs_queue.put({'handler_id': self.handler_id,
                                               'body_arguments': request.data,
-                                              'remote_ip': remote_ip,
-                                              'indexes': ['*']})  # * means that EverythingEqual object is used
+                                              'remote_ip': remote_ip})
         )
         self.logger.debug(f'MakeJob RESPONSE: {response}', extra={'hid': self.handler_id})
         return Response(response)
--- jobsmanager_transit/worker/jobsmanager_transit_worker.py
+++ jobsmanager_transit/worker/jobsmanager_transit_worker.py
@@ -1,24 +1,37 @@
 import sys
-# todo update complex_rest and remove
-sys.path.insert(0, '.')
-from core.settings.base import PLUGINS_DIR
-sys.path.insert(1, PLUGINS_DIR)
 import asyncio
 import os
 import logging
 from configparser import ConfigParser
 from psycopg2.pool import ThreadedConnectionPool
-from jobsmanager_transit.ot_simple_rest.jobs_manager.jobs import Job
 from message_broker import AsyncConsumer as Consumer
 from json import loads
 from jobsmanager_transit.wrappers.simple_request import SimpleRequest
 from jobsmanager_transit.ot_simple_rest.utils.primitives import EverythingEqual
 from jobsmanager_transit.ot_simple_rest.jobs_manager.manager import JobsManager
 
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s] %(message)s",
+    handlers=[
+        # logging.FileHandler("./logs/jobsmanager_transit/out.log"),
+        logging.StreamHandler(sys.stdout)
+    ]
+)
+
+logger_osr = logging.getLogger('ladybug')
+logger_osr.info('ladybug')
+
 
 async def async_range(count):
     for i in range(count):
-        yield(i)
+        yield i
+
+
+def convert_to_bin(data):
+    for d in data.values():
+        d[0] = d[0].encode('utf-8')
+
 
 async def main():
     # # # # # #  Configuration section  # # # # # # #
@@ -49,15 +62,11 @@ async def main():
     async with Consumer(topic, value_deserializer=loads) as consumer:
         async for msg in consumer:
             print('received message')
-            # вызываем await makejob у менеджера
-            # async for
             job_description = msg.value
             print(job_description)
+            if type(job_description['body_arguments']['original_otl'][0]) is str:
+                convert_to_bin(job_description['body_arguments'])
             request = SimpleRequest(job_description['body_arguments'], job_description['remote_ip'])
-
-            request.body_arguments['original_otl'][0] = request.body_arguments['original_otl'][0].encode('utf-8')
-            request.body_arguments['username'][0] = request.body_arguments['username'][0].encode('utf-8')
-            request.body_arguments['sid'][0] = request.body_arguments['sid'][0].encode('utf-8')
 
             await manager.make_job(
                 hid=job_description['handler_id'],
@@ -68,9 +77,9 @@ async def main():
             async for _ in async_range(queue_size):
                 job = await manager.jobs_queue.get()
                 print('Got a job from queue')
-                # await job.start_make()
-                asyncio.create_task(job.start_make())
-                await asyncio.sleep(0)
+                await job.start_make()
+                await asyncio.sleep(0.001)
+
 
 if __name__ == '__main__':
     loop = asyncio.get_event_loop()
